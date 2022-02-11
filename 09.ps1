5b9492d2e98db77d061e3f746d691063016f00aa
a19xavrodbue <95306359+a19xavrodbue@users.noreply.github.com>
2022-02-11 19:00:04 +0100

    Practica

--- 09.ps1
+++ 09.ps1
@@ -1,10 +1,8 @@
 #si dos numeros passats per parametre a l'script son iguals mostra OK. Si son diferents mostra KO
 
-$valor1 = Read-Host "Primer valor"
-$valor2 = Read-Host "Segon valor"
-if ($value1 -eq $value2){
-    Write-Output "OK"
+if ($args[0] -eq $args[1]){
+        Write-Output "OK"
 }
 else{
     Write-Output "KO"
-} 
+}
