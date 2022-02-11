820dcd590ef321e571ac31f1ced06a15086422b1
a19xavrodbue <95306359+a19xavrodbue@users.noreply.github.com>
2022-02-11 19:06:56 +0100

    Practica Final

--- 02.ps1
+++ 02.ps1
@@ -1,3 +1,3 @@
 #Imprimeix per pantalla el dia de l'any (dels 365) en què estem. Per tant, estarà entre 1 i 365
 
-(get-date -year 2022 -mounth 2 -day 8).dayofyear 
+Write-Output (Get-Date).DayOfYear 
